2080590d354264f7b6cf9d54dcea00e51d50ad02
Cooper Edmunds <cooper6789@gmail.com>
2018-12-23 16:44:16 -0500

    Fix S3 implementation, make cron more efficient, and add chains endpoint

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import boto3
 from flask import Flask, jsonify, request
 from datetime import datetime
 from collections import defaultdict
+from queue import Queue
 
 from sportsreference.ncaab.boxscore import Boxscores
 
@@ -13,39 +14,85 @@ class GameNode:
    def __init__(self, game, date):
       self.team = game["winning_name"]
       self.opponent = game["losing_name"]
-
-      if(game["home_name"] == game["winning_name"]):
-          self.winner_score = game["home_score"]
-          self.loser_score = game["away_score"]
-      else:
-          self.winner_score = game["away_score"]
-          self.loser_score = game["home_score"]
-
-      self.home_team = game["home_name"]
-      self.date = date
-
-   def __str__(self):
-     print("beat %s by %d @ %s" % opponent, winner_score - loser_score, home_team)
+      self.differential = abs(game["home_score"] - game["away_score"])
 
 
 @app.route('/wins', methods=['GET'])
 def get_wins():
-    request_params = request.values
-    team = request_params['team']
-
+    # Instantiate S3 client
     s3_client = boto3.client('s3',
                           aws_access_key_id=os.environ['aws_access_key_id'],
                           aws_secret_access_key=os.environ['aws_secret_access_key'],
                           region_name=os.environ['region']
                           )
 
-    s3_client.download_file('graphs-cbbchaingame', 'current_season_graph.json', 'current_season.json')
+    request_params = request.values
+    team = request_params['team']
 
+    # Get graph data from S3
+    s3_client.download_file(os.environ['bucket'], 'current_season_graph.json', 'current_season.json')
     data = json.loads(open('current_season.json').read())
+    os.remove('current_season.json')
 
     response = {"updated": data["updated"], "wins" : data["graph"][team]}
     return json.dumps(response)
 
+
+@app.route('/chains', methods=['GET'])
+def get_chains():
+    # Instantiate S3 client
+    s3_client = boto3.client('s3',
+        aws_access_key_id=os.environ['aws_access_key_id'],
+        aws_secret_access_key=os.environ['aws_secret_access_key'],
+        region_name=os.environ['region']
+    )
+
+    request_params = request.values
+    teamA = request_params['teamA']
+    teamB = request_params['teamB']
+    excluded_teams = json.loads(request_params['excluded_teams'])
+
+    # Get graph from S3
+    s3_client.download_file(os.environ['bucket'], 'current_season_graph.json', 'current_season.json')
+    graph = json.loads(open('current_season.json').read())['graph']
+    os.remove('current_season.json')
+
+    def make_chains(teamA, target, exclude, max_num, link_limit):
+        answers = []
+        history_queue = Queue()
+
+        history_queue.put([teamA])
+
+        while(len(answers) < max_num and history_queue.qsize() > 0):
+            history = history_queue.get()
+
+            if(len(history) > link_limit): return answers
+
+            last_team = history[len(history) - 1]
+
+            if(last_team == target):
+                answers.append(" -> ".join(history))
+                continue
+
+            if(last_team not in graph):
+                continue
+
+            wins = graph[last_team]
+
+            for win in wins:
+                opponent = win['opponent']
+                if(opponent not in history and opponent not in exclude):
+                    new_history = history[:]
+                    new_history.append(opponent)
+                    history_queue.put(new_history)
+
+        return answers
+
+    a_to_b_chains = make_chains(teamA, teamB, excluded_teams, 30, 8)
+    b_to_a_chains = make_chains(teamB, teamA, excluded_teams, 30, 8)
+
+    response = {"a_to_b_chains": a_to_b_chains, "b_to_a_chains" : b_to_a_chains}
+    return json.dumps(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- crons/makegraph.py
+++ crons/makegraph.py
@@ -2,36 +2,53 @@ import sys
 import os
 import json
 import boto3
-from datetime import datetime
+import pytz
+from datetime import datetime, tzinfo
 from collections import defaultdict
 
 from sportsreference.ncaab.boxscore import Boxscores
 
 class GameNode:
-   def __init__(self, game, date):
-      # self.team = game["winning_name"]
+   def __init__(self, game):
+      self.team = game["winning_name"]
       self.opponent = game["losing_name"]
-
-      if(game["home_name"] == game["winning_name"]):
-          self.winner_score = game["home_score"]
-          self.loser_score = game["away_score"]
-      else:
-          self.winner_score = game["away_score"]
-          self.loser_score = game["home_score"]
-
-      self.home_team = game["home_name"]
-      self.date = date
-
-   def __str__(self):
-     print("beat %s by %d @ %s" % opponent, winner_score - loser_score, home_team)
+      self.differential = abs(game["home_score"] - game["away_score"])
 
 
-now = datetime.now()
-all_games_data = Boxscores(datetime(now.year, 12, 15), now)
+# Instantiate S3 client
+s3_client = boto3.client('s3',
+                      aws_access_key_id=os.environ['aws_access_key_id'],
+                      aws_secret_access_key=os.environ['aws_secret_access_key'],
+                      region_name=os.environ['region']
+                      )
+
+last_updated = datetime(datetime.utcnow().year, 8, 15)
+current_graph = {}
+try:
+  # Get the graph stored in S3 and write it to a local file
+  s3_client.download_file("graphs-cbbchaingame", 'current_season_graph.json', 'current_graph.json')
+  data = json.loads(open('current_graph.json').read())
+  os.remove('current_graph.json')
+
+  # Parse last updated date
+  last_updated_str = data['updated']
+  last_updated = datetime.strptime(last_updated_str, "%x at %I:%M %p")
+
+  current_graph = data['graph']
+except:
+  pass
+
+# Get the games between the last updated date and now
+all_games_data = Boxscores(last_updated, datetime.utcnow())
 data = all_games_data.games
 
-graph = defaultdict(list)
+new_graph = defaultdict(list)
 
+# Add all of the nodes from the current graph
+for team in current_graph.keys():
+    new_graph[team].extend(current_graph[team])
+
+# Iterate over all of the new games just fetched
 for date in data.keys():
     games = data[date]
 
@@ -39,19 +56,18 @@ for date in data.keys():
         winner = game["winning_name"]
 
         # Only add D1 games
-        if(game["non_di"] != True):
-            game_record = GameNode(game, date)
-            graph[winner].append(game_record.__dict__)
+        if(game["non_di"] != True and game['winning_name'] != None):
+            game_record = GameNode(game)
+            # If this game isn't already in the graph, add it
+            if(game_record.__dict__ not in new_graph[winner]):
+                new_graph[winner].append(game_record.__dict__)
 
-dict_to_write = {"updated":datetime.now().strftime("%x at %I:%M %p"), "graph":graph}
+# Convert the new graph and time to a json file
+dict_to_write = {"updated":datetime.utcnow().strftime("%x at %I:%M %p"), "graph":new_graph}
 json_to_write = json.dumps(dict_to_write)
+open('new_graph.json', 'w').write(json_to_write)
 
-open('current_season.json', 'w').write(json_to_write)
+# Upload the json file to S3
+s3_client.upload_file('new_graph.json', "graphs-cbbchaingame", 'current_season_graph.json')
 
-s3_client = boto3.client('s3',
-                      aws_access_key_id=os.environ['aws_access_key_id'],
-                      aws_secret_access_key=os.environ['aws_secret_access_key'],
-                      region_name=os.environ['region']
-                      )
-
-s3_client.upload_file('current_season.json', 'graphs-cbbchaingame', 'current_season_graph.json')
+os.remove('new_graph.json')
