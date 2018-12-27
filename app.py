b7e5b3f2886b7218a07c8a0e3f3ebc30fd127b46
Cooper Edmunds <cooper6789@gmail.com>
2018-12-27 10:58:29 -0500

    Return data for graph from /chains endpoint

--- app.py
+++ app.py
@@ -57,30 +57,53 @@ def get_chains():
     graph = json.loads(open('current_season.json').read())['graph']
     os.remove('current_season.json')
 
-    def team_not_in_history(team, history):
-        for win in history:
+    def team_not_in_chain(team, chain):
+        for win in chain:
             if(win['l'] == team or win['w'] == team):
                 return False
         return True
 
     def make_chains(team_a, target, exclude, max_num, link_limit):
-        answers = []
-        history_queue = Queue()
+        chains = []
+        chains_queue = Queue()
 
+        edges_set = set()
+        edges = []
+
+        nodes = []
+
+        nodes_dict = {}
         team_a_wins = graph[team_a]
         for win in team_a_wins:
-            history_queue.put([win])
+            chains_queue.put([win])
 
-        while(len(answers) < max_num and history_queue.qsize() > 0):
-            history = history_queue.get()
+        while(len(chains) < max_num and chains_queue.qsize() > 0):
+            chain = chains_queue.get()
 
-            if(len(history) > link_limit): return answers
+            if(len(chain) > link_limit):
+                break
 
-            last_win = history[len(history) - 1]
+            last_win = chain[len(chain) - 1]
             opponent = last_win['l']
 
             if(opponent == target):
-                answers.append(history)
+                chains.append(chain)
+                for win in chain:
+                    winner = win['w']
+                    if winner not in nodes_dict:
+                        nodes.append({ "id": len(nodes_dict), "label": winner, "title": winner })
+                        nodes_dict[winner] = len(nodes_dict)
+
+                    loser = win['l']
+                    if loser not in nodes_dict:
+                        nodes.append({ "id": len(nodes_dict), "label": loser, "title": loser })
+                        nodes_dict[loser] = len(nodes_dict)
+
+                    edge_string = winner + "/-/" + loser
+                    if edge_string not in edges_set:
+                        edges_set.add(edge_string)
+                        edges.append({"from": nodes_dict[winner], "to": nodes_dict[loser]})
+
                 continue
 
             if(opponent not in graph):
@@ -90,49 +113,17 @@ def get_chains():
 
             for win in wins:
                 opponent_2 = win['l']
-                if(team_not_in_history(opponent_2, history) and opponent_2 not in exclude):
-                    new_history = history[:]
-                    new_history.append(win)
-                    history_queue.put(new_history)
+                if(team_not_in_chain(opponent_2, chain) and opponent_2 not in exclude):
+                    new_chain = chain[:]
+                    new_chain.append(win)
+                    chains_queue.put(new_chain)
 
-        return answers
+        return {"chains":chains, "nodes":nodes, "edges":edges}
 
-    # def make_chains(teamA, target, exclude, max_num, link_limit):
-    #     answers = []
-    #     history_queue = Queue()
-    #
-    #     history_queue.put([teamA])
-    #
-    #     while(len(answers) < max_num and history_queue.qsize() > 0):
-    #         history = history_queue.get()
-    #
-    #         if(len(history) > link_limit): return answers
-    #
-    #         last_team = history[len(history) - 1]
-    #
-    #         if(last_team == target):
-    #             answers.append(" > ".join(history))
-    #             continue
-    #
-    #         if(last_team not in graph):
-    #             continue
-    #
-    #         wins = graph[last_team]
-    #
-    #         for win in wins:
-    #             opponent = win['opponent']
-    #             if(opponent not in history and opponent not in exclude):
-    #                 new_history = history[:]
-    #                 new_history.append(opponent)
-    #                 history_queue.put(new_history)
-    #
-    #     return answers
+    a_to_b_data = make_chains(teamA, teamB, excluded_teams, 30, 7)
+    b_to_a_data = make_chains(teamB, teamA, excluded_teams, 30, 7)
 
-
-    a_to_b_chains = make_chains(teamA, teamB, excluded_teams, 30, 7)
-    b_to_a_chains = make_chains(teamB, teamA, excluded_teams, 30, 7)
-
-    response = {"a_to_b_chains": a_to_b_chains, "b_to_a_chains" : b_to_a_chains}
+    response = {"a_to_b": a_to_b_data, "b_to_a" : b_to_a_data}
     return json.dumps(response)
 
 if __name__ == '__main__':
